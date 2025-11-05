72890c804a84755fa2226baa1e8bb1c931cc7bc7
mwawrzen <w.w@op.pl>
2025-11-05 14:27:49 +0100

    feat: create tests for PESEL validation

---
+++ src/utils/validate-pesel.test.ts
@@ -0,0 +1,21 @@
+import { PeselError } from "./types";
+import { validatePESEL } from "./validate-pesel";
+
+describe('Testing PESEL validation', () => {
+
+  test('PESEL is correct', () => {
+    expect(validatePESEL('03230906092')).toBe(PeselError.NONE);
+  });
+
+  test('Wrong date format', () => {
+    expect(validatePESEL('03233206092')).toBe(PeselError.DATE);
+  });
+
+  test('Wrong check digit', () => {
+    expect(validatePESEL('03230906094')).toBe(PeselError.CHECK_DIGIT);
+  });
+
+  test('Wrong length', () => {
+    expect(validatePESEL('03230906')).toBe(PeselError.LENGTH);
+  });
+});
