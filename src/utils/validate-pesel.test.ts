777bbdcb8fd972c4aa2e400dadea7791c6a91b5f
mwawrzen <w.w@op.pl>
2025-11-05 21:20:57 +0100

    feat: update tests names

--- src/utils/validate-pesel.test.ts
+++ src/utils/validate-pesel.test.ts
@@ -3,19 +3,19 @@ import { validatePESEL } from "@/utils/validate-pesel";
 
 describe('Testing PESEL validation', () => {
 
-  test('PESEL is correct', () => {
+  test('Should return NONE error for correct data', () => {
     expect(validatePESEL('03230906092')).toBe(PeselError.NONE);
   });
 
-  test('Wrong date format', () => {
+  test('Should return DATE error for wrong date', () => {
     expect(validatePESEL('03233206092')).toBe(PeselError.DATE);
   });
 
-  test('Wrong check digit', () => {
+  test('Should return CHECK_DIGIT error for wrong check digit', () => {
     expect(validatePESEL('03230906094')).toBe(PeselError.CHECK_DIGIT);
   });
 
-  test('Wrong length', () => {
+  test('Should return LENGTH error for wrong length', () => {
     expect(validatePESEL('03230906')).toBe(PeselError.LENGTH);
   });
 });
