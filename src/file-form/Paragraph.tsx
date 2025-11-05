23646f17e1eb2bb79e55e6fe232d7bdb3fc59114
mwawrzen <w.w@op.pl>
2025-11-05 13:08:38 +0100

    feat: create

---
+++ src/file-form/Paragraph.tsx
@@ -0,0 +1,17 @@
+function Paragraph({ text }: { text: string | null}) {
+
+  if (!text)
+    return null;
+
+  return (
+    <section className="text-left mt-6">
+      {
+        text.split('\n').map(line => line.trim()).map((line, i) => (
+          line.length ? <p key={i}>{line}</p> : <br key={i} />
+        ))
+      }
+    </section>
+  );
+}
+
+export default Paragraph;
